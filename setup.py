1b0bb1771b87296ee4cd01ec2acb253def7620d1
Geir Skjotskift <geir@mnemonic.no>
2019-09-23 13:47:57 +0200

    renamed to underscore naming convention

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
             'act-uploader = act.workers.generic_uploader:main_log_error',
             'act-url-shorter-unpack = act.workers.url_shorter_unpack:main_log_error',
             'act-shadowserver-asn = act.workers.shadowserver_asn:main_log_error',
-            'act-ip-asn-history = act.workers.ip-asn-history:main_log_error',
+            'act-ip-asn-history = act.workers.ip_asn_history:main_log_error',
             'act-veris = act.workers.veris:main_log_error',
             'act-worker-config = act.workers.worker_config:main',
         ]
